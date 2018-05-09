7dea303d0c18db0c9fc627537a41fcbd81621d2f
user154 <user154@gmail.com>
2018-05-09 20:34:34 +0300

    Final version

--- Board.cpp
+++ Board.cpp
@@ -9,7 +9,7 @@
  * Default Constructor init all board to '.'
  */
 Board::Board() {
-    for (int i = 0; i <this->size ; ++i) {
+    for (int i = 0; i <this->size ; i++) {
             board[i] = new MyChar();
     }
 }
@@ -20,9 +20,9 @@ Board::Board() {
 Board::Board(int s) {
     this->size = s;
     this->board = new MyChar*[size];
-    for (int i = 0; i <this->size ; ++i) {
+    for (int i = 0; i <this->size ; i++) {
         this->board[i] = new MyChar();
-        for (int j = 1; j <this->size ; ++j) {
+        for (int j = 1; j <this->size ; j++) {
             this->board[i][j].setChar('.');
         }
     }
@@ -35,9 +35,9 @@ Board::Board(int s) {
 Board::Board(const Board& b) {
     this->size = b.size;
     board = new MyChar*[size];
-    for (int i = 0; i <this->size ; ++i) {
+    for (int i = 0; i <this->size ; i++) {
         board[i] = new MyChar();
-        for (int j = 0; j <this->size ; ++j) {
+        for (int j = 0; j <this->size ; j++) {
             board[i][j] = b.board[i][j];
         }
     }
@@ -53,8 +53,8 @@ Board &Board::operator=(char c) {
         throw ex;
     }
     else{
-        for (int i = 0; i <this->size ; ++i) {
-            for (int j = 0; j <this->size ; ++j) {
+        for (int i = 0; i <this->size ; i++) {
+            for (int j = 0; j <this->size ; j++) {
                 this->board[i][j]=c;
             }
         }
@@ -77,7 +77,7 @@ Board &Board::operator=(const Board& other) {
             for (int i = 0; i <size ; i++) {
                 board[i] = new MyChar[size];
             }
-            for (int i = 0; i < this->size; ++i) {
+            for (int i = 0; i < this->size; i++) {
                 for (int j = 0; j < this->size; j++) {
                     this->board[i][j] = MyChar{other.board[i][j].getChar()};
                 }
@@ -116,8 +116,8 @@ MyChar& Board::operator[](list<int> l) {
 bool Board::operator==(const Board &l) const{
     if(size != l.size) return false;
     else{
-        for (int i = 0; i <size; ++i) {
-            for (int j = 0; j <size ; ++j) {
+        for (int i = 0; i <size; i++) {
+            for (int j = 0; j <size ; j++) {
              if(board[i][j] != l.board[i][j]) return false;
             }
         }
